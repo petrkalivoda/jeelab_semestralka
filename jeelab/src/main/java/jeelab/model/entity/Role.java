cc9d8b89c9f8ab0a1293ec9348cac0547587e930
Oscar Hernández <oscar.hernandez@improvisio.cz>
2016-05-23 23:42:07 +0200

    added role entity

---
+++ jeelab/src/main/java/jeelab/model/entity/Role.java
@@ -0,0 +1,53 @@
+package jeelab.model.entity;
+
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role_table") //role is sql reserved keyword
+public class Role implements JpaEntity {
+
+    private static final long serialVersionUID = 2907932664849397798L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    
+    @ManyToMany(targetEntity = User.class)
+    private Set users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set users) {
+        this.users = users;
+    }
+
+}
--- jeelab/src/main/java/jeelab/model/entity/User.java
+++ jeelab/src/main/java/jeelab/model/entity/User.java
@@ -8,81 +8,93 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Set;
+import javax.persistence.ManyToMany;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_table") //user is sql reserved keyword
 public class User implements JpaEntity {
 
-	private static final long serialVersionUID = 2907932664849397798L;
+    private static final long serialVersionUID = 2907932664849397798L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
-	
-	@Column(name = "firstname")
-	private String firstname;
-	
-	@Column(name = "lastname")
-	private String lastname;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-	@Column(name = "email", unique = true)
-	private String email;
-	
-	@JsonIgnore
-	@Column(name = "password")
-	private String password;
-	
-	@Column(name = "active")
-	private boolean active = true;
-	
-	public Long getId() {
-		return id;
-	}
+    @Column(name = "firstname")
+    private String firstname;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @Column(name = "lastname")
+    private String lastname;
 
-	public String getFirstname() {
-		return firstname;
-	}
+    @Column(name = "email", unique = true)
+    private String email;
 
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
+    @JsonIgnore
+    @Column(name = "password")
+    private String password;
 
-	public String getLastname() {
-		return lastname;
-	}
+    @Column(name = "active")
+    private boolean active = true;
 
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
+    @ManyToMany(targetEntity = Role.class)
+    private Set roles;
 
-	public String getEmail() {
-		return email;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-	public boolean isActive() {
-		return active;
-	}
+    public String getLastname() {
+        return lastname;
+    }
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-	
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set roles) {
+        this.roles = roles;
+    }
+
 }
-
