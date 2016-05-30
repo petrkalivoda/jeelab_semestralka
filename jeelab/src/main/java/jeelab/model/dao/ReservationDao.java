13f7fd91f98a5df658223cd2465b0a1c22c26bc6
Oscar Hernández <oscar.hernandez@improvisio.cz>
2016-05-30 15:39:27 +0200

    finished reservation dao

--- jeelab/src/main/java/jeelab/model/dao/ReservationDao.java
+++ jeelab/src/main/java/jeelab/model/dao/ReservationDao.java
@@ -3,10 +3,14 @@ package jeelab.model.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import jeelab.model.builder.ReservationBuilder;
 import jeelab.model.entity.Reservation;
+import jeelab.model.entity.SportsCentre;
+import jeelab.view.ReservationForm;
 
 /**
  * Rezervace
@@ -16,6 +20,9 @@ import jeelab.model.entity.Reservation;
 @Stateless
 public class ReservationDao {
 
+	@Inject
+	private ReservationBuilder reservationBuilder;
+	
 	@PersistenceContext(unitName = "jee")
     private EntityManager manager;
 	
@@ -24,16 +31,57 @@ public class ReservationDao {
     	manager.flush();
     }
 	
+	public void updateReservation(long id, ReservationForm form) {
+		Reservation reservation = manager.find(Reservation.class, id);
+		if(reservation != null){
+			reservationBuilder
+				.date(form.getDate())
+				.from(form.getFrom())
+				.to(form.getTo())
+				.user(form.getUser())
+				.sportsCentreFacility(form.getCentreFacility())
+				.build(reservation);
+		}
+	}
+	
+	public void deleteReservation(long id) {
+		Reservation reservation = manager.find(Reservation.class, id);
+		if (reservation != null){
+			manager.remove(reservation);
+		}
+	}
+	
+	/**
+	 * Get reservation by id
+	 * @param id
+	 * @return
+	 */
+	public Reservation getReservation(Long id){
+    	return manager.find(Reservation.class, id);
+    }
+	
 	/**
 	 * Vsechny Rezervace
 	 * @param userId
 	 * @return
 	 */
-	//TODO strankovani
-    public List<Reservation> getReservations() {
+    public List<Reservation> getReservations(Long max, Long offset) {
         return manager
-                .createQuery("select reservation from Reservation reservation", Reservation.class)
+                .createQuery("select reservation from Reservation reservation "
+                			+ "order by reservation.date"
+                			+ "limit :offset,:max",
+                			Reservation.class)
+                .setParameter("offset", offset)
+                .setParameter("max", max)
                 .getResultList();
+    }
+    
+    /**
+     * Total number of reservations
+     * @return
+     */
+    public Long getReservationsCount(){
+    	return (Long) manager.createQuery("select count(reservation) from Reservation reservation").getSingleResult();
     }
 	
 	/**
@@ -41,12 +89,27 @@ public class ReservationDao {
 	 * @param userId
 	 * @return
 	 */
-	//TODO strankovani
-    public List<Reservation> getUserReservations(Long userId) {
+    public List<Reservation> getUserReservations(Long userId, Long max, Long offset) {
         return manager
-                .createQuery("select reservation from Reservation reservation where reservation.user.id = ?", Reservation.class)
-                .setParameter(1, userId)
+                .createQuery("select reservation from Reservation reservation where reservation.user.id = :userId"
+                			+ "order by reservation.date"
+                			+ "limit :offset,:max", 
+                			Reservation.class)
+                .setParameter("userId", userId)
+                .setParameter("offset", offset)
+                .setParameter("max", max)
                 .getResultList();
+    }
+    
+    /**
+     * Total number of reservation for user
+     * @param userId
+     * @return
+     */
+    public Long getUserReservationsCount(Long userId){
+    	return (Long) manager.createQuery("select count(reservation) from Reservation reservation where reservation.user.id = :userId")
+    			.setParameter("userId", userId)
+    			.getSingleResult();
     }
 	
 }
