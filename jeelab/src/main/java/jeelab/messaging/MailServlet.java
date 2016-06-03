8cad66906a898438e5374c6730f7c65759ba63ab
Oscar Hernández <oscar.hernandez@improvisio.cz>
2016-06-03 06:15:31 +0200

    do something with jms

--- jeelab/src/main/java/jeelab/messaging/MailServlet.java
+++ jeelab/src/main/java/jeelab/messaging/MailServlet.java
@@ -1,26 +1,53 @@
 package jeelab.messaging;
 
-import javax.ejb.EJB;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.inject.Inject;
 import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSException;
+import javax.naming.NamingException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.logging.Logger;
 
 @JMSDestinationDefinition(name = "java:jboss/jms/queue/exampleQueue",
 className = "javax.jms.Queue",
 destinationName="queue1234",
 description="My Queue", 
 interfaceName = "java:jboss/jms/queue/exampleQueue")
-@WebServlet(urlPatterns = {"/TestServlet"})
+@WebServlet(urlPatterns = {"/jms"})
 public class MailServlet extends HttpServlet {
  
- @EJB MailProducer sender;
+	@Inject
+	MailProducer sender;
  
- @EJB MailConsumer receiver;
+	@Inject
+	MailConsumer receiver;
 
 
- public void doGet(HttpServletRequest request, HttpServletResponse response) {
-	 //TODO: DO SHIT HERE 
- }
+	 public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		 response.setContentType("text/html;charset=UTF-8");
+	     try (PrintWriter out = response.getWriter()) {
+	         out.println("<html>");
+	         out.println("<head>");
+	         out.println("<title>JMS2 Send Message</title>");            
+	         out.println("</head>");
+	         out.println("<body>");
+	         out.println("<h1>JMS2 Send/Receive Message using JMS2 " + request.getContextPath() + "</h1>");
+	         String m = "Hello there";
+	         sender.sendMail(m);
+	         out.format("Message sent: %1$s.<br>", m);
+	         out.println("Receiving message...<br>");
+	         String message = receiver.startReceiver();
+	         out.println("Message rx: " + message);
+	         out.println("</body>");
+	         out.println("</html>");
+		} catch (JMSException | NamingException | IOException e) {
+			Logger.getLogger(MailServlet.class).error(e);
+		}
+	 }
 }
